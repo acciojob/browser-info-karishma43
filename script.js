71f92e41892d9182c9f6706eda42031784761ecf
karishma43 <120738287+karishma43@users.noreply.github.com>
2023-06-30 22:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+et div=document.createElement("div");
+div.id='browser-info';
+document.body.append(div);
+div.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
